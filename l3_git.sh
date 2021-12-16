3cb8d1b90e16fbb3077afad0d5c4000b81e0366a
Vadim Repetukha <95625975+Shpr0t123@users.noreply.github.com>
2021-12-16 12:24:17 +0300

    Update l3_git.sh

--- l3_git.sh
+++ l3_git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is a comment
-mkdir Repetukha && cd Repetukha && mkdir VARA && mkdir VARB && mkdir VARC && mkdir VARD && cd VARB && mkdir GR1 && cd GR1 && echo "group number: 131; date: 06.12.21; email: repeukha123@gmail.com" > Vadim.txt && cd .. && cd .. && cd VARC && mkdir GR2 && cd GR2 && echo "group number: 131; date: 06.12.21; email: repeukha123@gmail.com" > Repetukha.txt 
+mkdir Repetukha1 && cd Repetukha && mkdir VARA && mkdir VARB && mkdir VARC && mkdir VARD && cd VARB && mkdir GR1 && cd GR1 && echo "group number: 131; date: 06.12.21; email: repeukha123@gmail.com" > Vadim.txt && cd .. && cd .. && cd VARC && mkdir GR2 && cd GR2 && echo "group number: 131; date: 06.12.21; email: repeukha123@gmail.com" > Repetukha.txt 
